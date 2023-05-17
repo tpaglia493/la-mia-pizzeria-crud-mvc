cc3532d8f3e28aa09e9145edfa27d073106ec386
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-17 15:00:05 +0200

    added contructor to pizzaModel

--- LaMiaPizzeria/Models/PizzaModel.cs
+++ LaMiaPizzeria/Models/PizzaModel.cs
@@ -1,6 +1,28 @@
-﻿namespace LaMiaPizzeria.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeria.Models
+
 {
     public class PizzaModel
     {
+        public int Id { get; set; }
+        [MaxLength(40)]
+        public string Name { get; set; }
+        [Column(TypeName ="text")]
+        public string Description { get; set; }
+        [MaxLength(300)]
+        public string ImgSource { get; set; }
+
+        public float Price { get; set; }
+
+        public PizzaModel(string name, string description, string imgSource, float price) 
+        { 
+            Name = name; 
+            Description = description; 
+            ImgSource = imgSource; 
+            Price = price; 
+        }
     }
 }
