8d460837c1904c811021d683b3dab32eeeac259f
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-17 15:36:47 +0200

    added  EF database - la rivoluzione industriale e le sue conseguenze sono state la rovina dell'umanità

---
+++ LaMiaPizzeria/DataBase/PizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.DataBase
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<PizzaModel> Pizze { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzaDb;Integrated Security=True;TrustServerCertificate=True");
+    }
+    }
+}
