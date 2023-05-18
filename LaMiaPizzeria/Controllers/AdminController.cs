1dabc25ea61c4be64530e002826d3d14c41477a7
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-18 16:27:14 +0200

    added page to modify menu

---
+++ LaMiaPizzeria/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using LaMiaPizzeria.DataBase;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ModifyMenu()
+        {
+            using(PizzaContext db = new PizzaContext()) 
+            { 
+                List<PizzaModel> pizze = new List<PizzaModel>();
+                return View("ModifyMenu",pizze);
+
+            }
+        }
+    }
+}
