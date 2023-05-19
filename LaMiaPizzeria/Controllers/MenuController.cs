6243f0c038e139473f0a03027c29d142522509cd
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-19 16:42:40 +0200

    added button to delete a pizza from database

--- LaMiaPizzeria/Controllers/MenuController.cs
+++ LaMiaPizzeria/Controllers/MenuController.cs
@@ -50,7 +50,7 @@ namespace LaMiaPizzeria.Controllers
 
         public IActionResult UpdatePizza(int id)
         {
-       
+
             using (PizzaContext db = new PizzaContext())
             {
 
@@ -73,19 +73,37 @@ namespace LaMiaPizzeria.Controllers
             {
                 PizzaModel? pizzaToModify = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
 
-                pizzaToModify.Description =modifiedPizza.Description;
+                pizzaToModify.Description = modifiedPizza.Description;
                 pizzaToModify.Price = modifiedPizza.Price;
                 pizzaToModify.ImgSource = modifiedPizza.ImgSource;
                 pizzaToModify.Name = modifiedPizza.Name;
 
                 db.SaveChanges();
-         
+
 
                 return RedirectToAction("ModifyMenu");
             }
 
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePizza(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                PizzaModel? pizzaToDelete = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+                if (pizzaToDelete != null)
+                {
+                    db.Remove(pizzaToDelete);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                { return NotFound("Non esiste una pizza da eliminare con questo id"); }
+            }
+        }
     }
 }
 
-        
+
